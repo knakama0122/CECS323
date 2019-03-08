f7aac845dda98f927af996340d21a5210defe088
Daniel Flores <Daniel.Flores03@student.csulb.edu>
2019-03-07 21:36:14 -0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,3 +1,11 @@
+CREATE TABLE WritingGroup (
+    groupName 	VARCHAR(50) NOT NULL,
+    headWriter 	VARCHAR(50),
+    yearFormed 	VARCHAR(4),
+    subject 	VARCHAR(20),
+    CONSTRAINT WritingGroup_pk PRIMARY KEY (groupName)
+);
+
 CREATE TABLE Publisher (
   publisherName VARCHAR(50) NOT NULL,
   publisherAddress VARCHAR(50),
